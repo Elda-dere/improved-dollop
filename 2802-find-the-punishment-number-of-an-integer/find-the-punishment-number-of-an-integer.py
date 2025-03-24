85fa7a44aaa7f4f82d78ef35c6c69d8818361270
Elda-dere <eldanaderese@gmail.com>
2025-03-25 03:05:05 +1200

    Time: 942 ms (49.97%) | Memory: 17.8 MB (71.39%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -1,33 +1,28 @@
 class Solution:
-    def find_partitions(self, start_index, current_sum, string_num, target):
-        # If we've used all digits, check if the sum is valid
-        if start_index == len(string_num):
-            return current_sum == target
+    def punishmentNumber(self, n: int) -> int:
+        def punshiment(start_index,current_sum,string,target):
+            if start_index==len(string) :
+                return target==current_sum
+            if current_sum > target:
+                return False
 
-        # If the current sum already exceeds target, stop
-        if current_sum > target:
+            for i in range(start_index,len(string)):
+
+                appened=string[start_index:i+1]
+                app_int=int(appened)
+
+                if punshiment(i+1,current_sum+app_int,string,target):
+                    return True
             return False
 
-        # Try all possible splits
-        for current_index in range(start_index, len(string_num)):
-            current_string = string_num[start_index : current_index + 1]
-            addend = int(current_string)
 
-            # Recur with updated sum and index
-            if self.find_partitions(current_index + 1, current_sum + addend, string_num, target):
-                return True
+        res=0
+        for i in range(1,n+1):
+            string=str(i**2)
 
-        return False
+            if punshiment(0,0,string,i):
+                res+=i**2
+        return res
+        
 
-    def punishmentNumber(self, n: int) -> int:
-        punishment_num = 0
 
-        for current_num in range(1, n + 1):
-            square_num = current_num * current_num
-            string_num = str(square_num)
-
-            # Check if valid split exists
-            if self.find_partitions(0, 0, string_num, current_num):
-                punishment_num += square_num
-
-        return punishment_num
